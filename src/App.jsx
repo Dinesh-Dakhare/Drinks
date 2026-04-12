f66f5ff3293b591b48715197a204ca32297c4930
Dinesh <dineshdakhare22@gmail.com>
2026-04-12 12:28:44 +0530

    project completed...

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,18 @@ import Cocktails from "./components/Cocktails";
 import About from "./components/About";
 import Art from "./components/Art";
 import Menu from "./components/Menu";
+import Contact from "./components/Contact";
 gsap.registerPlugin(ScrollTrigger, SplitText);
 const App = () => {
   return (
     <main>
       <Navbar />
       <Hero />
-      <Cocktails/>
-      <About/>
-      <Art/>
-      <Menu/>
+      <Cocktails />
+      <About />
+      <Art />
+      <Menu />
+      <Contact />
     </main>
   );
 };
